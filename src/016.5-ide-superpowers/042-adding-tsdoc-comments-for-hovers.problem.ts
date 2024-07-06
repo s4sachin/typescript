ed279fb3551f55573115c5bf3abb4c53aea1dc96
s4sachin <sachinpurohit.text@google.com>
2024-07-06 17:05:14 +0530

    Add comments using TSDoc

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,20 @@
+/**
+ * Add two numbers
+ * @param a 
+ * @param b 
+ * 
+ * 
+ * @example
+ * myFunction(1,2)
+ * 
+ * @returns 
+ * 3
+ */
+
+
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
+
+
+myFunction(2,3)
