cce01d3abc2de3acfca09ef22880a2595f772058
s4sachin <sachinpurohit.text@gmail.com>
2024-08-28 14:55:07 +0530

    TS Classes

--- src/030-classes/110-receiving-arguments-to-constructor.problem.ts
+++ src/030-classes/110-receiving-arguments-to-constructor.problem.ts
@@ -4,6 +4,11 @@ class CanvasNode {
   x = 0;
   y = 0;
 
+  constructor(opts? :{x: number, y: number}){
+    this.x = opts?.x || 0;
+    this.y = opts?.y || 0;
+  }
+
   move(x: number, y: number) {
     this.x = x;
     this.y = y;
